379e3b26d67fb6bb3dd1d1b4c3104af5caff5517
Khrystyna Shalavylo <shalavilo52@gmail.com>
2023-11-12 02:26:57 +0200

    added animation

---
+++ js/modal.js
@@ -0,0 +1,8 @@
+const modal = document.querySelector('.backdrop');
+const modalBtnOpen = document.querySelector('.open-modal');
+const modalBtnClose = document.querySelector('.close-modal');
+
+const toggleModal = () => modal.classList.toggle('is-hidden');
+
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);

9f2f9f9631861d224cd15628b7d0e8ed17dbd06d
Khrystyna Shalavylo <shalavilo52@gmail.com>
2023-11-17 04:13:25 +0200

    added carousel logic and add adaptation

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,16 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.open-menu');
 const menuBtnClose = document.querySelector('.close-menu');
+const menuBtnCloseTraditions = document.getElementById('close-menuTraditions');
+const menuBtnCloseChiefs = document.getElementById('close-menuChiefs');
+const menuBtnCloseFormats = document.getElementById('close-menuFormats');
+const menuBtnCloseContacts = document.getElementById('close-menuContacts');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+menuBtnCloseTraditions.addEventListener('click', toggleMenu);
+menuBtnCloseChiefs.addEventListener('click', toggleMenu);
+menuBtnCloseFormats.addEventListener('click', toggleMenu);
+menuBtnCloseContacts.addEventListener('click', toggleMenu);
--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.open-modal');
+const modalBtnOpenHero = document.getElementById('open-modal-hero');
 const modalBtnClose = document.querySelector('.close-modal');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
-
+    
+modalBtnOpenHero.addEventListener('click', toggleModal);
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
